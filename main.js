84c0b57832ccde32a67d4f8408f1c3021049985c
arush-hue <77105133+arush-hue@users.noreply.github.com>
2021-08-19 16:38:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xcg70yj1S/model.json',modelLoaded);
+}
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+function draw()
+{
+    image(video,0,0,300,300);
+    classifier.classify(video,gotResults);
+}
+function gotResults(error,results)
+{
+    if (error)
+    {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_span-o").innerHTML=results[0].label;
+        document.getElementById("result_span-a").innerHTML=results[0].confidence.toFixed(1);
+    }
+}
